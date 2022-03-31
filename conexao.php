84e716efdeb56dc221e360363cb26199cf32dd12
czfantin <73662317+czfantin@users.noreply.github.com>
2022-03-30 22:08:01 -0300

    Commit inicial

---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bc541fa9ef1bfe', 'a64fd8e0', 'heroku_c092168738cbe68') or die('Erro ao conectar ao banco de dados');
+
+
